ffc14796d3f018d23a27a9fb97f8af77f1c7c9ee
CarlosBouzasClase <carlos.bouzas@educa.madrid.org>
2020-11-17 21:08:36 +0100

    Trabajo en clase 17/11/2020

--- src/Bucles/sesion07While.DoWhile/Ejercicio6.java
+++ src/Bucles/sesion07While.DoWhile/Ejercicio6.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Ejercicio6 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int i = 0;
+int j;
+
+
+while (i <= 125) {
+    for (j = 1; j > 0; j--) {
+
+
+    }
+
+}
+
 
     }
 }
