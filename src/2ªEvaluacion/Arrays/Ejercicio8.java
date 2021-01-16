8733c02977d61d7275c43956259d7aa1e3d567a9
Bouzas1402 <carlos.bouzas@educa.madrid.org>
2021-01-16 14:30:09 +0100

    Ejercicios de los apuntes String, 16/01/2021

--- src/2ªEvaluacion/Arrays/Ejercicio8.java
+++ src/2ªEvaluacion/Arrays/Ejercicio8.java
@@ -1,8 +1,6 @@
 package Arrays;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Ejercicio8 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -13,26 +11,49 @@ public class Ejercicio8 {
         //
         //• Todos lo alumnos mayores de edad.
         //• Los alumnos mayores (los que tienen más edad)
+        int contadorEdad = 0;
 int n;
 do{
     System.out.println("Alumnos: ");
     n = sc.nextInt();
 }while(n <= 0);
 sc.nextLine(); //Para meter numeros y String esto es una linea vacia que te "Limpia el escaner".
-
         String [] nombre = new String[n];
         int[] edades = new int[n];
 //con el asterisco propone crear una variable String respuesta; si respuesta = * salir del bucle
         //pido nombre y edades
         //si no tuvieramos la exigencia del asterisco seria asi:
+        String respuesta;
         for (int i = 0; i < n; i++) {
             System.out.println("Nombre:");
-            nombre [i] = sc.nextLine(); //Esto lee strings encadenados (Lineas enteras)
+            respuesta = sc.nextLine(); //Esto lee strings encadenados (Lineas enteras)
+            if (respuesta.equals("*")) {
+                break; } else {
+                nombre[i] = respuesta;
+            }
+             //si quiero comparar dos string pongo el string.equals y el string que quiero comparar
             System.out.println(("Edad:"));
             edades [i] = sc.nextInt();
-            sc.nextLine();//Seimpre que pedimos valores por teclado que combinen numeros y String metemos esta linea
+            sc.nextLine();//Siempre que pedimos valores por teclado que combinen numeros y String metemos esta linea
         }
+System.out.println("Alumnos con mayoria de edad:");
+        for (int i = 0; i < n; i++) {
+            if (edades [i] >= 18){
+                System.out.println(nombre[i] + "(" + edades[i] + ").");
+            }
+        }
+        //Alinear edades
+        int maximo = edades[0];
+        int pos = 0;
+        for (int i = 0; i < edades.length; i++) {
+            if (edades[i] > maximo){
+                maximo = edades[i];
+                        pos = i;
+            }
+        }
+        System.out.println("Alumno de mas edad: " + nombre[pos] + " edad: " + maximo);
         System.out.println(Arrays.toString(nombre));
         System.out.println(Arrays.toString(edades));
     }
 }
+
---
+++ src/2ªEvaluacion/String/prueba.java
@@ -0,0 +1,76 @@
+package String;
+import java.util.Scanner;
+import java.lang.String;
+public class prueba {
+    public static void main(String[] args){
+        ejercicioPrueba7();
+    }
+    public static void ejercicioPrueba2() {
+/*¿Qué valor tendrá la variable str despues de ejecutar el siguiente código?
+String str = "a simple string";
+str = str.replace("a", "A SIMPLE").substring(2, 8);*/
+        String str = "a simple string";
+        str = str.replace("a", "A SIMPLE").substring(2, 8);
+System.out.println(str);
+    }
+public static void ejercicioPrueba3() {
+        /* Reemplazar caracteres: escribe un programa que lea una cadena de texto y reemplace todas
+las ocurrencias de la letra ‘a’ con la letra ‘b’. A continuación debe imprimir la cadena
+resultante. */
+String cadena = "a";
+cadena = cadena.replace("a", "b");
+System.out.println(cadena);
+}
+public static void ejercicioPrueba4() {
+        /* Comprobar si termina con...: escribe un programa que lea el nombre de una ciudad y
+compruebe si el nombre termina con “burgo”. El programa debe imprimir true o false. */
+    Scanner sc = new Scanner(System.in);
+System.out.println("Introduzca el nombre de una ciudad:");
+String ciudad = sc.next();
+boolean terminoCiudad = ciudad.endsWith("burgo");
+System.out.println(terminoCiudad);
+}
+
+public static void ejercicioPrueba5(){
+        /* Comprobar si comienza con... ignorando mayúsculas y minúsculas: escribe un programa
+que compruebe si una cadena dada comienza por “J”, sin que tenga en cuenta si está en
+mayúsculas o minúsculas. El programa debe imprimir true o false. */
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escriba una cadena de texto:");
+    String cadena = sc.next();
+System.out.println((String.valueOf(cadena.charAt(0))).equalsIgnoreCase("j"));
+    }
+    public static void ejercicioPrueba6() {
+/*Comparar cadenas ignorando los espacios en blanco: escribe un programa que lea dos
+cadenas y las compare sin tener en cuenta los espacios en blanco. El programa debería
+imprimir true si son iguales, false en caso contrario.*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba una cadena de texto:");
+        String cadena1 = sc.nextLine();
+        System.out.println("Escriba una cadena de texto:");
+        String cadena2 = sc.nextLine();
+        String cadena1SinEspacios = cadena1.replace(" ","");
+        String cadena2SinEspacios = cadena2.replace(" ","");
+        System.out.println(cadena1SinEspacios.equals(cadena2SinEspacios));
+    }
+    public static void ejercicioPrueba7 () {
+        /*Extraer una subcadena: escribe un programa que lea una cadena y dos números enteros
+para indicar los límites inferior y superior respectivamente de un rango dentro de las
+posiciones de los caracteres de la cadena. Imprime la subcadena indicada por el rango: a)
+suponiendo que ambos números siempres son mayores o iguales a 0 y menores que la
+longitud de la cadena; b) Validando que sucede a).*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce una cadena de texto:");
+        String cadena = sc.nextLine();
+        int longitud = cadena.length();
+        System.out.println("Introduce un valor entre 0 y " + longitud + "(incluidos)");
+        int num1 = sc.nextInt();
+        System.out.println("Introduce un valor entre 0 y " + longitud + "(incluidos)");
+int num2 = sc.nextInt();
+sc.nextLine();
+cadena = cadena.substring(num1,num2);
+System.out.println(cadena);
+    }
+}
+
+
