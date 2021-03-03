dfd0ae174cc4d56fa38e787edb16690b095b3c6b
CarlosBouzasClase <carlos.bouzas@educa.madrid.org>
2021-03-03 16:25:21 +0100

    Clase del 03/03/2021

--- src/2ªEvaluacion/PrepararExamen/Seguridad/Edificio.java
+++ src/2ªEvaluacion/PrepararExamen/Seguridad/Edificio.java
@@ -1,5 +1,52 @@
 package PrepararExamen.Seguridad;
-
+import java.util.ArrayList;
 public class Edificio {
+
+    private String direccion;
+    private Integer codigoPostal;
+    ArrayList<Alarma> alarmas;
+
+    public Edificio(String direccion, Integer codigoPostal){
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        //creamos en el constructor el arraylist pero esta vacio
+        ArrayList<Alarma> alarmas = new ArrayList<>();
+        this.alarmas = alarmas;
+    }
+
+    @Override
+    public String toString() {
+        return "Edificio{" +
+                "direccion='" + direccion + '\'' +
+                ", codigoPostal=" + codigoPostal +
+                ", alarmas=" + alarmas +
+                '}';
+    }
+//crearAlarma(TipoAlerta): Alarma - Instancia una alarma y la devuelve.
+    public Alarma crearAlarma (TipoAlerta tipo){
+        Alarma a = new Alarma(tipo);
+        return a;
+    }
+//añadirAlarma(Alarma alarma): void – añade la alarma recibida al ArrayList de alarmas
+    public void añadirAlarma(Alarma a){
+        this.alarmas.add(a);
+    }
+
+    //quitarAlarma(int pos): void – quita la alarma de la posición indicada del ArrayList de
+    //alarmas
+
+    public void quitarAlarma (int pos){
+        this.alarmas.remove(pos);
+    }
+
+    //mostrarAlarmas(): void – imprime una frase: “las alarmas instaladas en este edificio
+    //son: “, y a continuación recorre el ArrayList de alarmas.
+
+    public void mostrarAlarmas(){
+        System.out.println("Las alarmas instaladas en este edificio son:");
+        for (int i = 0; i < alarmas.size(); i++) {
+            // System.out.println((i+1) + alarmas.get(i).tipo);
+        }
+    }
 
 }
--- src/2ªEvaluacion/PrepararExamen/Seguridad/Principal.java
+++ src/2ªEvaluacion/PrepararExamen/Seguridad/Principal.java
@@ -1,5 +1,7 @@
 package PrepararExamen.Seguridad;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         Sensor sensor1 = new Sensor(TipoAlerta.INCENDIO);
@@ -24,5 +26,25 @@ alarma1.mostrarSensores();
     alarma1.sensores.get(0).activar();
     System.out.println("Hacemos sonar la alarma");
 alarma1.sonar(sensor1);
+
+//Vamoa a crear un edificio pedido por teclado
+        Scanner sc =new Scanner(System.in);
+       /* System.out.println("Direccion del edificio a crear:");
+        String d = sc.nextLine();
+        System.out.println("Que codigo postal tiene el edificio a crear:");
+        Integer cp = sc.nextInt();
+        sc.nextLine(); //liberar el escaner al introducir un int
+        Edificio edificio1 = new Edificio(d,cp); */
+        Edificio edificio2 = new Edificio("Fernandez llamazares 14", 28045);
+        // System.out.println(edificio1.toString());
+        System.out.println(edificio2.toString());
+
+        Alarma alarma2 = edificio2.crearAlarma(TipoAlerta.INCENDIO);
+        edificio2.añadirAlarma(alarma2);
+        System.out.println(edificio2.toString());
+
+        edificio2.quitarAlarma(0);
+        System.out.println(edificio2.toString());
+
     }
 }
