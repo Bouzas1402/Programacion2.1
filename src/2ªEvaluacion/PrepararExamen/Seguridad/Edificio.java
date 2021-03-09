6f57adc1150432ae58aa295b9853899629f81948
Bouzas1402 <carlos.bouzas@educa.madrid.org>
2021-03-09 10:53:42 +0100

    09/03/2021 documentacion javadoc

--- src/2ªEvaluacion/PrepararExamen/Seguridad/Edificio.java
+++ src/2ªEvaluacion/PrepararExamen/Seguridad/Edificio.java
@@ -1,11 +1,23 @@
 package PrepararExamen.Seguridad;
 import java.util.ArrayList;
 public class Edificio {
-
+    /**
+     * Clase edificio que crea edificios para el paquete Seguridad
+     * @eutor Carlos Bouzas
+     * @version Seguridad 2.0.0
+     *
+     *
+     */
      String direccion;
      Integer codigoPostal;
     ArrayList<Alarma> alarmas;
 
+    /**
+     * Constructor de la clase Edificio, pide dos datos y genera un ArrayList de tipo alarma
+     * @param direccion es un String que asigna un valor al atributo direccion
+     * @param codigoPostal es un Integer que asigna un valor al atributo codigoPostal
+     * @since Seguridad 1.0.0
+     */
     public Edificio(String direccion, Integer codigoPostal){
         this.direccion = direccion;
         this.codigoPostal = codigoPostal;
@@ -26,11 +38,24 @@ public class Edificio {
                 '}';
     }
 //crearAlarma(TipoAlerta): Alarma - Instancia una alarma y la devuelve.
+
+    /**
+     * Metodo que recibe un TipoAlerta y se lo asigna a una Alarma
+     * @param tipo  de tipoAlerta
+     * @return una Alarma con valor del tipoAlerta introducido
+     * @since Seguridad 1.2.0
+     */
     public Alarma crearAlarma (TipoAlerta tipo){
         Alarma a = new Alarma(tipo);
         return a;
     }
 //añadirAlarma(Alarma alarma): void – añade la alarma recibida al ArrayList de alarmas
+
+    /**
+     * El metodo recibe una Alarma y la introduce en el ArrayList alarmas
+     * @param a de tipo Alarma
+     * @since Seguridad 1.1.2
+     */
     public void añadirAlarma(Alarma a){
         this.alarmas.add(a);
     }
@@ -38,6 +63,11 @@ public class Edificio {
     //quitarAlarma(int pos): void – quita la alarma de la posición indicada del ArrayList de
     //alarmas
 
+    /**
+     * Este metodo recoge un int que nos dira que posición del ArrayList alarmas debo borrar
+     * @param pos recibe un int que representa una pisición de un ArraList alarmas
+     * @since Seguridad 1.2.0
+     */
     public void quitarAlarma (int pos){
         this.alarmas.remove(pos);
     }
@@ -45,6 +75,10 @@ public class Edificio {
     //mostrarAlarmas(): void – imprime una frase: “las alarmas instaladas en este edificio
     //son: “, y a continuación recorre el ArrayList de alarmas.
 
+    /**
+     * Este metodo muestra las alarmas que hay en un Arraylist alarmas
+     * @since Seguridad 2.0.0
+     */
     public void mostrarAlarmas(){
         System.out.println("Las alarmas instaladas en este edificio son:");
         for (int i = 0; i < alarmas.size(); i++) {
